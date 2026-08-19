4a9995ed215ca7c4b7e8ef8efd2d2bced011133c
Juliano Caetano - Libertas Faculdade Integradas <julianosilva@libertas.edu.br>
2026-08-19 19:04:52 -0300

    Páginas exemplo feitas em sala de aula.

---
+++ Aulas/aula04/js/ex01.js
@@ -0,0 +1,10 @@
+const frm=document.querySelector("form")
+const resp=document.querySelector("h3")
+
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado.
+frm.addEventListener("submit", (e)=> {
+  const nome = frm.inNome.value // obtem valor cx texto
+  resp.innerText = `Olá ${nome}`
+
+  e.preventDefault() // evita o envio do form
+})
